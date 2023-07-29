ab2bc21fa72048a4162e3368aa24e1f00b090e9c
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-07-29 02:54:10 -0600

    importing-db-creating-utils-creating-random-quotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 
+import { useState } from 'react'
 import './App.css'
 
+/* importing json db */
+import quotes from './db/quote.json'
+
 function App() {
+
+  /* function to get random quotes */
+  const getRandom = (list) => {
+
+    /* random method from Math object which gives a random number  */
+    /* floor method from Math object which rounds down */
+    const randomIndex = Math.floor(Math.random() * list.length)
+
+    return list[randomIndex]
+  }
+
+  const [quote, setQuote] = useState(getRandom(quotes))
 
 
   return (
     <>
-
+      <h1>Entregable</h1>
     </>
   )
 }
